01e5c27407f772f5cd3876d9dad041f4fba6f0bf
Jonathan G. Underwood <jonathan.underwood@gmail.com>
2023-01-17 13:50:09 +0000

    Initial check-in of some files

---
+++ bash/.profile.d/20-python.sh
@@ -0,0 +1,68 @@
+# Requiired environment variables for virtualenvwrapper
+export WORKON_HOME=$HOME/.virtualenvs
+export PROJECT_HOME=$HOME/Projects
+
+# Find the latest python
+if command -v python3 > /dev/null 2>&1 ; then
+    python=$(command -v python3)
+    pyver=$(python3 -c "import sys; print('{0}.{1}'.format(sys.version_info.major, sys.version_info.minor))")
+elif command -v python > /dev/null 2>&1 ; then
+    python=$(command -v python)
+    pyver=$(python -c "import sys; print('{0}.{1}'.format(sys.version_info.major, sys.version_info.minor))")
+else
+    return 0
+fi
+
+# Don't do this: it will override what the virtualenv activate script does to
+# put the virtualenv python at the start of the path when the virtualenv is
+# activated.
+# alias python="$python"
+
+# Add the bin directory that is used for when a `pip install --user <pkg>`
+# command installs something executable.
+user_bindir="$HOME/Library/Python/$pyver/bin"
+export PATH="$user_bindir":"$PATH"
+
+# If we have installed virtualenvwrapper, source its setup file. But only do
+# this if not already in a virtualenv (e.g. in a peotery shell).
+wrapper=$(command -v virtualenvwrapper.sh)
+
+if [[ -f "$wrapper" ]] && [[ -z "$VIRTUAL_ENV" ]]; then
+    export VIRTUALENVWRAPPER_PYTHON="$python"
+    source "$wrapper"
+fi
+
+function reinstall_virtualenvwrapper {
+    # When Python is upgraded, it's necessary to reinstall virtualenvwrapper,
+    # because when it is installed it installs a hook loader for the discovered
+    # version of Python.
+    pip3 install --user --force-reinstall -U virtualenvwrapper
+}
+
+function poetry_venv_setup_dev_tools {
+    # This function adds packages to a poetry virtualenv that are required for
+    # spacemacs to use lsp mode for Python.
+    # See: https://develop.spacemacs.org/layers/+lang/python/README.html
+    poetry run pip install importmagic epc python-lsp-server[all] # pyright
+}
+
+function setup_jupyter {
+    pip install jupyter notebook jupyter_contrib_nbextensions
+    # Enable extensions. The --sys-prefix ensures that the extensions are
+    # configured in the current virtualenv rather than at a system level (which
+    # is what happens without this option).
+    jupyter contrib nbextension install --sys-prefix
+    # Extensions to enable. Note to list what is enabled, run:
+    # jupyter nbextension list
+    jupyter nbextension enable --sys-prefix collapsible_headings/main
+
+    pip install autopep8
+    jupyter nbextension enable --sys-prefix code_prettify/autopep8
+
+    pip install yapf
+    jupyter nbextension enable --sys-prefix code_prettify/code_prettify
+    # Don't enable lux for now - it's very buggy
+    # pip install lux-api
+    # jupyter nbextension install --py luxwidget
+    # jupyter nbextension enable --py luxwidget
+}
---
+++ bash/.profile.d/25-linuxify.sh
@@ -0,0 +1,163 @@
+# Homebrew packages for GNU uitilities etc that have the same name as OSX
+# utilities install them in the path with a "g" prefix. This file adds the paths
+# to the unprefixed commands to the PATH so that they are used instead of the
+# OSX default utilities. This is necessary because Homebrew was removed all
+# options from packages, includin g the --with-default-names option.
+
+function env_append {
+    # $1: environment variable name
+    # $2: string to append
+    # $3: delimiter to use between values
+
+    local -n e="$1"
+    local delim
+
+    if [[ -z "$3" ]]; then
+        delim=":"
+    else
+        delim="$3"
+    fi
+
+
+    if [[ -z "$e" ]]; then
+        export "$1"="$2"
+    else
+        export "$1"="$e""$delim""$2"
+    fi
+}
+
+function env_prepend {
+    # $1: environment variable name
+    # $2: string to prepend
+    # $3: delimiter to use between values
+
+    local -n e="$1"
+    local delim
+
+    if [[ -z "$3" ]]; then
+        delim=":"
+    else
+        delim="$3"
+    fi
+
+    if [[ -z "$e" ]]; then
+        export "$1"="$2"
+    else
+        export "$1"="$2""$delim""$e"
+    fi
+}
+
+function env_prepend_dir {
+    # $1: environment variable name
+    # $2: directory to prepend if it exists and is a directory
+    # $3: delimiter to use between values
+    # $4: prefix for path
+
+    local delim
+
+    if [[ -z "$3" ]]; then
+        delim=":"
+    else
+        delim="$3"
+    fi
+
+    if [[ -d "$2" ]]; then
+        env_prepend "$1" "$4$2" "$delim"
+    fi
+}
+
+function env_prepend_file {
+    # $1: environment variable name
+    # $2: directory to prepend if it exists and is a file
+    # $3: delimiter to use between values
+
+    local delim
+
+    if [[ -z "$3" ]]; then
+        delim=":"
+    else
+        delim="$3"
+    fi
+
+    if [[ -f "$2" ]]; then
+        env_prepend "$1" "$2" "$delim"
+    fi
+}
+
+if ! command -v brew > /dev/null 2>&1 ; then
+    # If brew is not installed, or not on the PATH, then don't do anything.
+    return 0
+fi
+
+brew_path="$(brew --prefix)"
+
+# List of packages which install their un-prefixed binaries outside of the PATH.
+# Might be better to discover these automatically using find:
+# find -L $(brew --prefix)/opt -type d -path */libexec/gnubin
+# But, does that throw up stuff we don't want on the PATH?
+brew_gnu_packages=(
+    "coreutils"
+    "ed"
+    "findutils"
+    "gawk"
+    "gnu-indent"
+    "gnu-sed"
+    "gnu-tar"
+    "gnu-which"
+    "grep"
+    "libtool"
+    "make"
+)
+
+# Note that, rather than hardcoding the path /opt/$pkg here, we could instead
+# use $(brew --prefix $pkg), but that's a very slow operation - 700 ms per
+# package. For some reason, a bare $(brew --prefix) is much faster at finding
+# the base install prefix.
+for pkg in "${brew_gnu_packages[@]}"; do
+    if [[ -d "$brew_path/opt/$pkg/libexec/gnubin" ]] ; then
+        env_prepend PATH "$brew_path/opt/$pkg/libexec/gnubin"
+        env_prepend_dir MANPATH "$brew_path/opt/$pkg/libexec/gnuman"
+    fi
+done
+
+# These packages don't follow the gnubin/gnuman pattern above. Instead, they
+# simply don't install symlinks into /usr/local/bin to their
+# /usr/local/opt/pkg/bin because OSX comes with older versions.
+brew_unlinked_packages=(
+    # Note: do not put binutils on the path as it will subtly break Python
+    # extensions. If you do at any point have this on the path and do any pip
+    # installs, you'll probably end up with broken wheels that won't install. In
+    # that case, do a `rm -rf ~/Library/Caches/pip` to prevent those broken
+    # wheels being used for subsequent installs, and then reinstall all
+    # packages.
+    # "binutils"
+    "bison"
+    "openssl"
+    "sqlite"
+    "zip"
+    "unzip"
+    "curl"
+)
+
+for pkg in "${brew_unlinked_packages[@]}"; do
+    if [[ -d "$brew_path/opt/$pkg/bin" ]]; then
+        env_prepend PATH "$brew_path/opt/$pkg/bin"
+        env_prepend_dir MANPATH "$brew_path/opt/$pkg/share/man"
+    fi
+done
+
+# Setup compiler to use some brew installed libraries, if they're present. This
+# is particularly important for allowing pip to build wheels locally. Note: an
+# alternative strategy here could be to look for a pkgconfig file in the
+# package, and if it's found add the library to LDFLAGS and CPPFLAGS. That would
+# save the hassle of maintaining the list manually.
+brew_libs=(
+    openssl
+    sqlite
+)
+
+for pkg in "${brew_libs[@]}"; do
+    env_prepend_dir LDFLAGS "$brew_path/opt/$pkg/lib" " " "-L"
+    env_prepend_dir CPPFLAGS "$brew_path/opt/$pkg/include" " " "-I"
+    env_prepend_file PKG_CONFIG_PATH "$brew_path/opt/$pkg/lib/pkgconfig"
+done
